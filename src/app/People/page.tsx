ff05be382a80747aed4c9d1f353ee2947ca3b8a9
Boaz Kaufman <bk1nyc@gmail.com>
2025-03-07 19:55:50 -0500

    Add constants and add the rest of the People page components

--- src/app/People/page.tsx
+++ src/app/People/page.tsx
@@ -1,9 +1,112 @@
 import React, { useEffect, useState } from 'react';
-import propTypes from 'prop-types';
 import axios from 'axios';
 import Link from "next/link";
  
-import { BACKEND_URL } from '../../constants';
+import { BACKEND_URL } from '@/app/constants.js';
+
+const PEOPLE_READ_ENDPOINT = `${BACKEND_URL}/people`;
+const PEOPLE_CREATE_ENDPOINT = `${BACKEND_URL}/people/create`;
+const ROLES_ENDPOINT = `${BACKEND_URL}/roles`;
+
+function AddPersonForm({
+  visible,
+  cancel,
+  fetchPeople,
+  setError,
+  roleOptions,
+}) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [role, setRole] = useState('');
+
+  const changeName = (event) => { setName(event.target.value); };
+  const changeEmail = (event) => { setEmail(event.target.value); };
+  const changeRole = (event) => { setRole(event.target.value); };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: name,
+      email: email,
+      roles: role,
+      affiliation: '',
+    }
+    axios.put(PEOPLE_CREATE_ENDPOINT, newPerson)
+      .then(fetchPeople)
+      .catch((error) => { setError(`There was a problem adding the person. ${error}`); });
+  };
+
+  if (!visible) return null;
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+      </label>
+      <input required type="text" id="name" value={name} onChange={changeName} />
+      <label htmlFor="email">
+        Email
+      </label>
+      <input required type="text" id="email" onChange={changeEmail} />
+      <select required name='role' onChange={changeRole}>
+        {
+          Object.keys(roleOptions).map((code) => (
+            <option key={code} value={code}>
+              {roleOptions[code]}
+            </option>
+          ))
+        }
+      </select>
+
+      <button type="button" onClick={cancel}>Cancel</button>
+      <button type="submit" onClick={addPerson}>Submit</button>
+    </form>
+  );
+}
+
+function ErrorMessage({ message }) {
+  return (
+    <div className="error-message">
+      {message}
+    </div>
+  );
+}
+
+function Person({
+  person,
+  fetchPeople,
+  roleMap,
+ }) {
+  const { name, email, roles } = person;
+
+  const deletePerson = () => {
+    axios.delete(`${PEOPLE_READ_ENDPOINT}/${email}`)
+      .then(fetchPeople)
+  }
+
+  return (
+    <div>
+      <Link to={email}>
+        <div className="person-container">
+          <h2>{name}</h2>
+          <p>
+            Email: {email}
+          </p>
+          <ul>
+            Roles: {roles.map((role) => (<li key={role}>{ roleMap[role] }</li>))}
+          </ul>
+        </div>
+      </Link>
+      <button onClick={deletePerson}>Delete person</button>
+    </div>
+  );
+}
+
+function peopleObjectToArray(Data) {
+  const keys = Object.keys(Data);
+  const people = keys.map((key) => Data[key]);
+  return people;
+}
+
 
 function People() {
   const [error, setError] = useState('');
